d811865de5f3315088161509eda32cca674a97ac
Larisa <laserova@yandex.ru>
2023-11-08 16:22:45 +0500

    task,ticket 10

---
+++ src/Pair.java
@@ -0,0 +1,17 @@
+public class Pair {
+    private final String author;
+    private final String book;
+
+    public Pair(String author, String book) {
+        this.author = author;
+        this.book = book;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getBook() {
+        return book;
+    }
+}
